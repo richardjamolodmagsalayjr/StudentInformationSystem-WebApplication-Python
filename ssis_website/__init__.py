1be393d4c0504630ba528ff92ea551d23cabc081
softwarejr <richardjr.magsalay@g.msuiit.edu.ph>
2021-09-25 22:44:48 +0800

    Minor changes in manage_student.html and add manage_student.html in templates

--- ssis_website/__init__.py
+++ ssis_website/__init__.py
@@ -8,10 +8,10 @@ def create_app():
 
     from .views import views
     # from .college import college
-    # from .course import course
+    from .course import course
     from .student import student
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(student, url_prefix="/")
-
+    app.register_blueprint(course, url_prefix="/")
     return app
--- ssis_website/course.py
+++ ssis_website/course.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 course = Blueprint('course', __name__)
+
+@course.route("/course")
+def manage_course():
+    return render_template("manage_course.html")
